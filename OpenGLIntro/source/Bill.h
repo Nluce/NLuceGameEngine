1edd1b6a4589844c4fc84db4ca0c6d5008a14c36
Nluce <noluce@me.com>
2015-02-04 11:33:02 -0800

    Worst code ever don't look at this
    blehhhhhhh

--- OpenGLIntro/source/Bill.h
+++ OpenGLIntro/source/Bill.h
@@ -16,16 +16,18 @@ public:
 		init();
 	}
 	virtual ~Bill();
-	vec3 platformList[7];
+	vec3 platformList[66];
 	
 
 	int speed = 100;
 	int numberOfRunFrames = 5;
 	int numberOfJumpFrames = 4;
+	int numberOfRunDiagonalFrames = 3;
+	int numberOfDeadFrames = 5;
 	int animationSpeedInFramesPerSecond = 10;
 	int jump = 200;
 	int drop = 10;
-	int screenHeight = 480;
+	int mapHeight = 2159;
 	double gravity = 250.0f;
 
 	bool isRunning = false;
@@ -34,7 +36,9 @@ public:
 	bool facingLeft = false;
 	bool isOnPlatform = false;
 	bool isDropping = false;
-
+	bool lookingUp = false;
+	bool lookingDown = false;
+	bool dead = false;
 
 	vec2 preMove;
 	vec2 postMove;
@@ -109,11 +113,17 @@ public:
 
 	std::vector<Shape *> shapesToDelete;
 
-	void move(float elapsedTime, GLFWwindow* window,float time)
-	{
-		
+	
 
+	void move(float elapsedTime, GLFWwindow* window, float time)
+	{
 		int stateSpace = glfwGetKey(window, GLFW_KEY_SPACE);
+		int stateLeft = glfwGetKey(window, GLFW_KEY_LEFT);
+		int stateDown = glfwGetKey(window, GLFW_KEY_DOWN);
+		int stateRight = glfwGetKey(window, GLFW_KEY_RIGHT);
+		int stateUp = glfwGetKey(window, GLFW_KEY_UP);
+		int stateShoot = glfwGetKey(window, GLFW_KEY_Z);
+
 		if (stateSpace == GLFW_PRESS && onTheGround)
 		{
 			//setVelocity(vec2(0, speed));
@@ -121,19 +131,19 @@ public:
 			isJumping = true;
 		}
 
-		int stateA = glfwGetKey(window, GLFW_KEY_A);
-		if (stateA == GLFW_PRESS)
+
+		if (stateLeft == GLFW_PRESS)
 		{
 			//setVelocity(vec2(- speed, 0));
 			velocity.x = -speed;
 			isRunning = true;
 			facingLeft = true;
 		}
-		
 
-		int stateS = glfwGetKey(window, GLFW_KEY_S);
+
+
 		isDropping = false;
-		if (stateS == GLFW_PRESS)
+		if (stateDown == GLFW_PRESS)
 		{
 			if (isOnPlatform)
 			{
@@ -141,12 +151,19 @@ public:
 				isJumping = true;
 				isDropping = true;
 			}
-			
 		}
 
+		if (stateUp == GLFW_PRESS && onTheGround)
+		{
+			lookingUp = true;
+		}
 
-		int stateD = glfwGetKey(window, GLFW_KEY_D);
-		if (stateD == GLFW_PRESS)
+		if (stateDown == GLFW_PRESS && onTheGround)
+
+		{
+			lookingDown == true;
+		}
+		if (stateRight == GLFW_PRESS)
 		{
 			velocity.x = speed;
 			isRunning = true;
@@ -155,7 +172,7 @@ public:
 
 		velocity.y -= gravity * elapsedTime;
 
-		if (stateD == GLFW_RELEASE && stateA == GLFW_RELEASE)
+		if (stateRight == GLFW_RELEASE && stateLeft == GLFW_RELEASE)
 		{
 			velocity.x = 0;
 			isRunning = false;
@@ -169,7 +186,7 @@ public:
 			velocity.y = 0;
 		}
 
-		
+
 
 		else
 		{
@@ -183,7 +200,7 @@ public:
 		if (!isDropping)
 		{
 
-			for (int i = 0; i < 7; i++)
+			for (int i = 0; i < 67; i++)
 			{
 				if (preMove.x > platformList[i].x && preMove.x < platformList[i].z)
 				{
@@ -198,7 +215,7 @@ public:
 
 				}
 			}
-		
+
 		}
 
 		if (isOnPlatform)
@@ -212,26 +229,61 @@ public:
 		{
 			position.x = 4;
 		}
-		if (position.x > 636)
+		if (position.x > 252)
 		{
-			position.x = 636;
+			position.x = 252;
 		}
 
 
 
-		position += velocity * elapsedTime; 
+		position += velocity * elapsedTime;
 
-		
+
 
 		//cout << velocity.y << endl;
 
 		int runFrame = int(time * animationSpeedInFramesPerSecond) % numberOfRunFrames;
+		int runDiagionalFrame = int(time * animationSpeedInFramesPerSecond) % numberOfRunDiagonalFrames;
 		int JumpFrame = int(time * animationSpeedInFramesPerSecond) % numberOfJumpFrames;
 
 		if (isRunning && !isJumping)
 		{
 
-			if (facingLeft)
+			if (facingLeft && lookingUp)
+			{
+				switch (runDiagionalFrame)
+				{
+				case 0:
+					setShape(contraShootUpLeftRunFrame1);
+					break;
+				case 1:
+					setShape(contraShootUpLeftRunFrame2);
+					break;
+				case 2:
+					setShape(contraShootUpLeftRunFrame3);
+
+					break;
+				}
+
+			}
+			else if (facingLeft && lookingDown)
+			{
+				switch (runDiagionalFrame)
+				{
+				case 0:
+					setShape(contraShootDownLeftRunFrame1);
+					break;
+				case 1:
+					setShape(contraShootDownLeftRunFrame2);
+					break;
+				case 2:
+					setShape(contraShootDownLeftRunFrame3);
+
+					break;
+				}
+
+			}
+			else if (facingLeft)
 			{
 				switch (runFrame)
 				{
@@ -250,11 +302,41 @@ public:
 				case 4:
 					setShape(contraRunLeftFrame5);
 					break;
+				}
+			}
 
+			else if (!facingLeft && lookingUp)
+			{
+				switch (runDiagionalFrame)
+				{
+				case 0:
+					setShape(contraShootUpRightRunFrame1);
+					break;
+				case 1:
+					setShape(contraShootUpRightRunFrame2);
+					break;
+				case 2:
+					setShape(contraShootUpRightRunFrame3);
+					break;
 				}
 
 			}
-			else
+			else if (!facingLeft && lookingDown)
+			{
+				switch (runDiagionalFrame)
+				{
+				case 0:
+					setShape(contraShootDownRightRunFrame1);
+					break;
+				case 1:
+					setShape(contraShootDownRightRunFrame2);
+					break;
+				case 2:
+					setShape(contraShootDownRightRunFrame3);
+					break;
+				}
+			}
+			else if (!facingLeft)
 			{
 				switch (runFrame)
 				{
@@ -273,9 +355,7 @@ public:
 				case 4:
 					setShape(contraRunFrame5);
 					break;
-
 				}
-
 			}
 		}
 		else if (isJumping)
@@ -318,19 +398,32 @@ public:
 				}
 			}
 		}
-		else
+		else if (facingLeft  && lookingUp)
 		{
-			if (facingLeft)
-			{
-				setShape(contraStandLeftFrame);
-			}
-			else
-			{
-				setShape(contraStandFrame);
-			}
+			setShape(contraShootStraightUpLeftFrame);
 		}
-	}
-	
+		else if (!facingLeft && lookingUp)
+		{
+			setShape(contraShootStraightUpRightFrame);
+		}
+		else if (facingLeft && lookingDown)
+		{
+			setShape(contraLayDownLeftFrame);
+		}
+
+		else if (!facingLeft && lookingDown)
+		{
+			setShape(contraLayDownRightFrame);
+		}
+		else if (facingLeft)
+				{
+					setShape(contraStandLeftFrame);
+				}
+				else
+				{
+					setShape(contraStandFrame);
+				}
+			}
 	Shape * makeShape(int left, int top, int width, int height)
 	{
 		Shape * shape = new Shape(contraSpriteSheet, left, top, width, height, BOTTOM_CENTER, false);
@@ -415,13 +508,7 @@ public:
 		contraRightDead = makeShape(311, 56, 41, 14);
 		contraLeftDead = makeMirrorShape(contraRightDead);
 
-		platformList[0] = vec3(0, screenHeight - 427, 77);
-		platformList[1] = vec3(77, screenHeight - 367, 310);
-		platformList[2] = vec3(302, screenHeight - 305, 389);
-		platformList[3] = vec3(392, screenHeight - 367, 640);
-		platformList[4] = vec3(77, screenHeight - 246, 310);
-		platformList[5] = vec3(77, screenHeight - 190, 230);
-		platformList[6] = vec3(230, screenHeight - 132, 640);
+		
 
 
 
@@ -429,7 +516,132 @@ public:
 		contraJumpLeftFrame2 = makeMirrorShape(contraJumpFrame2);
 		contraJumpLeftFrame3 = makeMirrorShape(contraJumpFrame3);
 		contraJumpLeftFrame4 = makeMirrorShape(contraJumpFrame4);
-	
+
+
+		platformList[0] = vec3(0, mapHeight - 2125, 256);
+		platformList[1] = vec3(160, mapHeight - 2092, 256);
+		platformList[2] = vec3(128, mapHeight - 2059, 222);
+		platformList[3] = vec3(0, mapHeight - 1996, 127);
+		platformList[4] = vec3(160, mapHeight - 1996, 256);
+		platformList[5] = vec3(32, mapHeight - 1965, 95);
+		platformList[6] = vec3(128, mapHeight - 1949, 256);
+		platformList[7] = vec3(96, mapHeight - 2028, 222);
+
+		platformList[8] = vec3(0, mapHeight - 1933, 64);
+
+		platformList[9] = vec3(192, mapHeight - 1917, 256);
+
+		platformList[10] = vec3(64, mapHeight - 1901, 159);
+
+		platformList[11] = vec3(0, mapHeight - 1870, 127);
+
+		platformList[12] = vec3(32, mapHeight - 1837, 191);
+
+		platformList[13] = vec3(0, mapHeight - 1805, 64);
+
+		platformList[14] = vec3(192, mapHeight - 1805, 256);
+
+		platformList[15] = vec3(32, mapHeight - 1758, 64);
+
+		platformList[16] = vec3(128, mapHeight - 1758, 256);
+
+		platformList[17] = vec3(32, mapHeight - 1725, 96);
+
+		platformList[18] = vec3(0, mapHeight - 1694, 128);
+
+		platformList[19] = vec3(160, mapHeight - 1678, 256);
+
+		platformList[20] = vec3(0, mapHeight - 1645, 256);
+
+		platformList[21] = vec3(0, mapHeight - 1597, 64);
+
+		platformList[22] = vec3(0, mapHeight - 1548, 128);
+
+		platformList[23] = vec3(32, mapHeight - 1485, 96);
+
+		platformList[24] = vec3(64, mapHeight - 1518, 256);
+
+		platformList[25] = vec3(128, mapHeight - 1453, 256);
+
+		platformList[26] = vec3(32, mapHeight - 1420, 128);
+
+		platformList[27] = vec3(0, mapHeight - 1390, 64);
+
+		platformList[28] = vec3(0, mapHeight - 1341, 256);
+		//missed this first time
+		platformList[63] = vec3(192, mapHeight - 1310, 256);
+
+		platformList[29] = vec3(160, mapHeight - 1278, 256);
+		// this too
+		platformList[64] = vec3(0, mapHeight - 1264, 64);
+
+		platformList[30] = vec3(128, mapHeight - 1245, 223);
+
+		platformList[31] = vec3(32, mapHeight - 1230, 96);
+
+		platformList[32] = vec3(0, mapHeight - 1197, 64);
+
+		platformList[33] = vec3(192, mapHeight - 1182, 256);
+
+		platformList[34] = vec3(32, mapHeight - 1132, 128);
+
+		platformList[35] = vec3(160, mapHeight - 1132, 256);
+
+		platformList[36] = vec3(0, mapHeight - 1102, 96);
+
+		platformList[37] = vec3(160, mapHeight - 1036, 192);
+
+		platformList[38] = vec3(128, mapHeight - 989, 192);
+
+		platformList[39] = vec3(160, mapHeight - 958, 223);
+
+		platformList[40] = vec3(0, mapHeight - 926, 32);
+
+		platformList[41] = vec3(64, mapHeight - 926, 160);
+
+		platformList[42] = vec3(128, mapHeight - 894, 192);
+
+		platformList[43] = vec3(64, mapHeight - 846, 223);
+
+		platformList[44] = vec3(32, mapHeight - 814, 128);
+
+		platformList[45] = vec3(64, mapHeight - 767, 160);
+
+		platformList[46] = vec3(192, mapHeight - 767, 256);
+
+		platformList[47] = vec3(192, mapHeight - 733, 223);
+
+		platformList[48] = vec3(160, mapHeight - 702, 256);
+
+		platformList[49] = vec3(32, mapHeight - 687, 128);
+
+		platformList[50] = vec3(0, mapHeight - 638, 96);
+
+		platformList[51] = vec3(192, mapHeight - 558, 256);
+
+		platformList[52] = vec3(161, mapHeight - 526, 256);
+
+		platformList[53] = vec3(0, mapHeight - 494, 223);
+
+		platformList[54] = vec3(128, mapHeight - 462, 256);
+
+		platformList[55] = vec3(0, mapHeight - 446, 96);
+
+		platformList[56] = vec3(96, mapHeight - 414, 190);
+
+		platformList[57] = vec3(0, mapHeight - 382, 128);
+
+		platformList[58] = vec3(160, mapHeight - 366, 256);
+
+		platformList[59] = vec3(32, mapHeight - 334, 96);
+
+		platformList[60] = vec3(128, mapHeight - 300, 256);
+
+		platformList[61] = vec3(0, mapHeight - 255, 256);
+
+		platformList[62] = vec3(0, mapHeight - 207, 256);
+
+
 		setShape(contraRunFrame1);
 	}
 
