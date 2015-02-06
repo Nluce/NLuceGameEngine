a0d0758c5113f7c22515a914675fda3c9a18ee0d
GloinTheDark <sluce@mac.com>
2015-02-06 13:36:36 -0800

    Added mirror flag to Sprite class

--- NLuceGameEngine/Sprite.h
+++ NLuceGameEngine/Sprite.h
@@ -14,14 +14,14 @@
 
 using namespace glm;
 
-static const glm::vec3 zAxis(0, 0, 1);
+static const vec3 zAxis(0, 0, 1);
 
 class Sprite
 {
-	
+protected:
 	float rotation; // around z axis
 	Shape * shape;
-	
+	bool mirror;
 
 public:
 	Sprite();
@@ -59,20 +59,26 @@ public:
 		rotation = newRotation;
 	}
 
-	void draw(const mat4 & projection, int matrixUniformID)
+	void draw(const mat4 & matrixIn, int matrixUniformID)
 	{
 		{
-			// first we need to rotate the model
-			glm::mat4 rotationMatrix = glm::rotate(glm::mat4(), rotation, zAxis);
+			mat4 out = matrixIn;
+
+
 
 			// then we move it to where we want it centered on the screen
-			glm::mat4 translationMatrix = glm::translate(glm::mat4(), vec3(position.x, position.y, 0.0f));
+			out = translate(out, vec3(position.x, position.y, 0.0f));
 
-			// then we apply the orthographic projection to put it into opengl coordinates
-			glm::mat4 mvp = projection * translationMatrix * rotationMatrix;
+			// first we need to rotate the model
+			out = rotate(out, rotation, zAxis);
+
+			if (mirror)
+			{
+				out = scale(out, vec3(-1, 1, 1));
+			}
 
 			//send our mvp matrix to the shader
-			glUniformMatrix4fv(matrixUniformID, 1, GL_FALSE, &mvp[0][0]);
+			glUniformMatrix4fv(matrixUniformID, 1, GL_FALSE, &out[0][0]);
 		}
 
 		shape->draw();
