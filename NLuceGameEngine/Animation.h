477e4b9d7adf928b374c67727f353d58e4f27c7e
GloinTheDark <sluce@mac.com>
2015-02-06 13:36:06 -0800

    Added Animation class.
    Simplifies animations

--- NLuceGameEngine/Animation.h
+++ NLuceGameEngine/Animation.h
@@ -1,2 +1,32 @@
+#pragma once
+
 #include "Shape.h"
 
+#include <vector>
+#include <iostream>
+
+class Animation {
+	std::vector<Shape *> frames;
+	float framesPerSecond = 10.0f;
+
+public:
+	Animation(){}
+
+	void addFrame(Shape* frame)
+	{
+		frames.push_back(frame);
+	}
+
+	Shape * getCurrentFrame(float timeInSeconds)
+	{
+		int size = frames.size();
+		if (size < 1)
+		{
+			std::cerr << "Animation has no frames" << std::endl;
+			return 0;
+		}
+		int frameIndex = int(timeInSeconds * framesPerSecond) % size;
+		return frames[frameIndex];
+	}
+
+};
